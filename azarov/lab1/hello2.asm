70389834817aed4b05e35fadd1e409a0eed96ead
Maxim0382 <71723565+Maxim0382@users.noreply.github.com>
2021-09-12 16:15:37 +0300

    upload files lab1

---
+++ azarov/lab1/hello2.asm
@@ -0,0 +1,52 @@
+; HELLO2 - Учебная программа N2  лаб.раб.#1 по дисциплине "Архитектура компьютера"
+;          Программа использует процедуру для печати строки
+;
+;      ТЕКСТ  ПРОГРАММЫ
+
+EOFLine  EQU  '$'         ; Определение символьной константы
+                          ;     "Конец строки"
+
+; Стек  программы
+
+ASSUME CS:CODE, SS:AStack
+
+AStack    SEGMENT  STACK
+          DW 12 DUP('!')    ; Отводится 12 слов памяти
+AStack    ENDS
+
+; Данные программы
+
+DATA      SEGMENT
+
+;  Директивы описания данных
+
+HELLO     DB 'Hello Worlds!', 0AH, 0DH,EOFLine
+GREETING  DB 'Student from 0382 - $'
+DATA      ENDS
+
+; Код программы
+
+CODE      SEGMENT
+; Процедура печати строки
+WriteMsg  PROC  NEAR
+          mov   AH,9
+          int   21h  ; Вызов функции DOS по прерыванию
+          ret
+WriteMsg  ENDP
+
+; Головная процедура
+Main      PROC  FAR
+          push  DS       ;\  Сохранение адреса начала PSP в стеке
+          sub   AX,AX    ; > для последующего восстановления по
+          push  AX       ;/  команде ret, завершающей процедуру.
+          mov   AX,DATA             ; Загрузка сегментного
+          mov   DS,AX               ; регистра данных.
+          mov   DX, OFFSET HELLO    ; Вывод на экран первой
+          call  WriteMsg            ; строки приветствия.
+          mov   DX, OFFSET GREETING ; Вывод на экран второй
+          call  WriteMsg            ; строки приветствия.
+          ret                       ; Выход в DOS по команде,
+                                    ; находящейся в 1-ом слове PSP.
+Main      ENDP
+CODE      ENDS
+          END Main
