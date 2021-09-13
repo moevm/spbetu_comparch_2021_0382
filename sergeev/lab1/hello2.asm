7bff573cf09c96db4280ec21c1d287158118817f
dimasergeev02 <justbringit16@yandex.ru>
2021-09-13 23:20:00 +0300

    lab1 done

---
+++ sergeev/lab1/hello2.asm
@@ -0,0 +1,37 @@
+EOFLine  EQU  '$'        
+
+ASSUME CS:CODE, SS:AStack
+
+AStack    SEGMENT  STACK
+          DW 12 DUP('!')
+AStack    ENDS
+
+
+DATA      SEGMENT
+
+HELLO     DB 'Hello Worlds!', 0AH, 0DH,EOFLine
+GREETING  DB 'Student from 0382 - Sergeev D.A. $'
+DATA      ENDS
+
+CODE      SEGMENT
+WriteMsg  PROC  NEAR
+          mov   AH,9
+          int   21h 
+          ret
+WriteMsg  ENDP
+
+Main      PROC  FAR
+          push  DS      
+          sub   AX,AX
+          push  AX
+          mov   AX,DATA            
+          mov   DS,AX               
+          mov   DX, OFFSET HELLO   
+          call  WriteMsg           
+          mov   DX, OFFSET GREETING
+          call  WriteMsg            
+          ret               
+
+Main      ENDP
+CODE      ENDS
+          END Main
