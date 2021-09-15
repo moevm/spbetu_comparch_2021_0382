d3ea2ca9c54be4150d4b839f3bcdd94f4d18db68
DanilFlint <didenkod2010@gmail.com>
2021-09-15 15:07:59 +0300

    didenko_lab1

---
+++ didenko/lab1/hello1.asm
@@ -0,0 +1,18 @@
+
+   DOSSEG                                
+   .MODEL  SMALL                           
+   .STACK  100h                             
+   .DATA                                     
+Greeting  LABEL  BYTE                         
+   DB 'Hi Im student Danil Didenko',13,10,'$'
+   .CODE                            
+   mov  ax, @data                  
+   mov  ds, ax                      
+   mov  dx, OFFSET Greeting          
+                            
+DisplayGreeting:
+   mov  ah, 9            
+   int  21h                
+   mov  ah, 4ch                  
+   int  21h                       
+   END
