eb6348b47a56384ef96114ecd7b344576a9fe879
Lyubava0382 <71721397+Lyubava0382@users.noreply.github.com>
2021-11-08 19:14:11 +0300

    Krivencova_lab3

--- lab3/lb3.asm
+++ lab3/lb3.asm
@@ -24,26 +24,14 @@ Main PROC FAR
  mov DS,AX
  
  ;Entering data
- mov a,0
- mov b,0
- mov i,0
- mov k,0
+ mov a,1
+ mov b,2
+ mov i,3
+ mov k,4
  
  mov AX, a
  cmp AX, b
  jg f1_first
- jle f1_second
-
-f1_first:
-    mov AX, i
-    shl AX, 1 ; = 2i
-    shl AX, 1 ; = 4i
-    mov i1, -3
-    sub i1, AX ; = -3 - 4i
-    mov AX, k
-    cmp AX, 0
-    jl f2_first
-    jmp f3_second
 
 f1_second:
     mov AX, i
@@ -54,9 +42,40 @@ f1_second:
     add i1, AX ; = -10 + 6i
     mov AX, k
     cmp AX, 0
-    jl f2_second
-    jmp f3_second
+    jge f3_second
 
+f2_second:
+    mov AX, i
+    shl AX, 1 ; = 2i
+    add AX, i ; = 3i
+    mov i2, 6
+    add i2, AX ; = 3i + 6
+
+i2abs:
+      neg i2 ; = |i2|
+js i2abs
+
+i1abs:
+      neg i1
+js i1abs ; = |i1|
+
+f3_first:
+    js i2abs
+    mov AX,i1
+    add AX,i2 ; = |i1| + |i2|
+    mov result, AX
+    ret
+
+f1_first:
+    mov AX, i
+    shl AX, 1 ; = 2i
+    shl AX, 1 ; = 4i
+    mov i1, -3
+    sub i1, AX ; = -3 - 4i
+    mov AX, k
+    cmp AX, 0
+    jge f3_second
+  
 f2_first:
     mov AX, i
     shl AX, 1 ; = 2i
@@ -67,26 +86,6 @@ f2_first:
     sub i2, AX ; = 4 - 6i
     jmp f3_first
 
-f2_second:
-    mov AX, i
-    shl AX, 1 ; = 2i
-    add AX, i ; = 3i
-    mov i2, 6
-    add i2, AX ; = 3i + 6
-    jmp f3_first
- i1abs:
-      neg i1
-  i2abs:
-      neg i2 ; = |i2|
-
-f3_first:
-  js i1abs ; = |i1|
-  js i2abs
-    mov AX,i1
-    add AX,i2 ; = |i1| + |i2|
-    mov result, AX
-    ret
-  
 f3_second:
   js i1abs ; = |i1|
     mov AX, i1
@@ -94,7 +93,7 @@ f3_second:
     jge case
     mov result, 6
     ret
- 
+
 case:
     mov result, AX
     ret
