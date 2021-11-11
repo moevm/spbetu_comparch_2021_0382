a4643bce30bb02941dffe230bc9a72d7c27d3d26
Xenia <ksandrstudy@gmail.com>
2021-11-11 11:18:26 +0300

    changes

--- andryushchenko/lab4/lb4.cpp
+++ andryushchenko/lab4/lb4.cpp
@@ -12,47 +12,40 @@ int main()
     setlocale(LC_ALL, "Russian");
 
 
-    wchar_t initial_msg[] = L"Студент гр.0382 Андрющенко К.С \n2) Формирование выходной строки только из цифр и латинских букв входной строки.";
+     wchar_t initial_msg[] = L"Студент гр.0382 Андрющенко К.С \n2) Формирование выходной строки только из цифр и латинских букв входной строки.";
 
     wcout << initial_msg << endl;
-    
     cin.getline(str_input, 81);
-
     __asm {
-        push  ds;  Сохранение адреса начала PSP в стеке
-        pop es
         mov esi, offset str_input
         mov edi, offset str_out
-        
+        mov ecx, '\0'
 
         symbol_selection:
             lodsb; копирует один байт из памяти по адресу DS : ESI(32) в AL
             cmp al, '\0'
             je end_line
-            cmp al, 0x7A; Расширенные символы ASCII Win - 1251 кириллица + спец.символы
-            jbe number_or_latin_character; если символ не является символом русского языка и спец.символами
-            jmp symbol_selection; выбираем следующий символ
+            cmp al, 'z'; Расширенные символы ASCII Win - 1251 кириллица + спец.символы
+            ja symbol_selection; если символ является символом русского языка и спец.символами
+            
 
         number_or_latin_character :
-            cmp al, 0x61
+            cmp al, 'a'
             jae write_symbol; строчные символы английского языка
-            cmp al, 0x5A
+            cmp al, 'Z'
             ja symbol_selection; выбираем следующий символ
-            cmp al, 0x41
+            cmp al, 'A'
             jae write_symbol; строчные символы английского языка
-            cmp al, 0x39
+            cmp al, '9'
             ja symbol_selection; выбираем следующий символ
-            cmp al, 0x30
+            cmp al, '0'
             jae write_symbol; числа
             ; иначе
             jmp symbol_selection; выбираем следующий символ
 
-        write_symbol:
+        write_symbol :
             stosb; сохраняет регистр AL в ячейке памяти по адресу ES : EDI.После выполнения команды, регистр EDI увеличивается на 1, если флаг DF = 0, или уменьшается на 1, если DF = 1.
-            cmp  ecx, [esi]
-            je end_line
             jmp symbol_selection; выбираем следующий символ
-
         end_line:
     };
     ofstream file(INPUT_FILENAME);
