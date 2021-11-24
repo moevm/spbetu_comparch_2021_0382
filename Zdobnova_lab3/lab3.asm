27eaac99acc0ed46a10e704ce34b3c3b652b5b0a
zdobnovakseniya <kzdobnova2001@gmail.com>
2021-11-25 02:30:31 +0300

    lab3: changed

--- Zdobnova_lab3/lab3.asm
+++ Zdobnova_lab3/lab3.asm
@@ -7,13 +7,13 @@ AStack 		ENDS
 DATA 		SEGMENT
 ;68 стр
 ; Директивы описания данных
-a 	DW 0
-b 	DW 0
-i 	DW 0
+a 	DW -3
+b 	DW 6
+i 	DW 5
 k 	DW 0
 i1 	DW 0
 i2 	DW 0
-res 	DW 0
+res DW 0
 
 DATA 	ENDS
 
@@ -32,62 +32,51 @@ Main PROC FAR
 	mov AX,a
 	mov BX,b
 	mov CX,i
-	cmp AX,BX
-	JG Above
+	sal CX,1  ;i = i * 2
+	cmp AX,b
 	JLE Less
 	
 Above:         ;a > b
 	add i1,15  ;i1 = 15
-	sal CX,1   ;i = i * 2
 	sub i2,2
 	add i2,CX  ;тк различие на 2
-	neg CX     ;i = - 2 * i
-	add i1,CX  ;i = 15 - 2i
+	sub i1,CX  ;i = 15 - 2i
 	mov CX, i1
 	CMP i1,0
-	JL Abs1
+	JGE F3
+	neg CX  ;берем i1 по модулю
 	JMP F3
 	
 Less:           ;a <= b
 	add i1,CX   ;i1 = i
-	sal CX,1    ;i = 2*i
 	add CX,i1   ;i = 3*i
 	mov i1,CX
 	add i1,4
 	add i2,2
-	neg CX
-	add i2,CX
+	sub i2,CX
 	mov CX, i1
 	CMP i1,0
-	JL Abs1
-	JMP F3
-	
-Abs1:           ;берем i1 по модулю
-	neg i1
-	
+	JGE F3
+	neg CX  ;берем i1 по модулю
+		
 F3:             
 	CMP k,0
 	JZ Zero
 	add res, CX
 	mov CX, i2
 	CMP CX,0
-	JL Abs2
-	add res,CX
-	JMP Finish
-	
-Abs2:         
+	JGE Ans
 	neg CX
-	add res, CX
-	JMP Finish
+	JMP Ans
 	
 Zero:
 	CMP CX,6
-	JL MinI
+	JL Ans
 	mov res, 6
 	JMP Finish
-	
-MinI:
-	mov res, CX
+
+Ans:
+	add res, CX
 
 Finish:
     ret                     
