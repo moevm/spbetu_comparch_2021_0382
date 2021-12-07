b62bbb9179a486ee31f4fe10a3387cae2f045563
Mikhail Efremov <jakutenshi@gmail.com>
2021-12-07 20:53:19 +0300

    add all students to the scripts

---
+++ fetch.sh
@@ -0,0 +1,32 @@
+git fetch Maxim0382          
+git fetch Hedy-dev           
+git fetch HoshiyamaSeizen    
+git fetch ngudow             
+git fetch detkovaannya       
+git fetch DanilFlint         
+git fetch Khail909           
+git fetch velonzy            
+git fetch Andrew-Zlobin      
+git fetch Fittern            
+git fetch YuriiKondratov     
+git fetch Lyubava0382        
+git fetch Mathless           
+git fetch kulikov02          
+git fetch SemyonLityagin     
+git fetch oksi-oksik         
+git fetch FavoriteCoffee     
+git fetch ev-osi             
+git fetch galinaOkhotnikova  
+git fetch ranatasha          
+git fetch Vadsxd             
+git fetch vasilysamulevich   
+git fetch DmitrySergeev02    
+git fetch Sergei4302         
+git fetch Timofey02          
+git fetch lizavetaChe        
+git fetch vasiliy2002        
+#git fetch  
+#git fetch  
+#git fetch  
+#git fetch  
+#git fetch  
