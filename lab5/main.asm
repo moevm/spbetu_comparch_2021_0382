e281ed15f0ef3e34d7c0801b177a72caa3010cc8
Vadsxd <vadim.sannikov.2018@mail.ru>
2021-12-09 12:33:13 +0300

    correct

--- lab5/main.asm
+++ lab5/main.asm
@@ -3,9 +3,6 @@ AStack  SEGMENT STACK
 AStack  ENDS
 
 DATA    SEGMENT
-	MESSAGE DB 'YES!', 0dh, 0ah, '$'
-	FINALLY DB 'Program Finished!$'
-	COUNTER DW 6
     KEEP_CS DW 0    ; для хранения сегмента вектора прерывания
     KEEP_IP DW 0    ; для смещения вектора прерывания
 DATA    ENDS
@@ -20,31 +17,51 @@ WriteMsg  PROC  NEAR
 WriteMsg  ENDP
 
 FUNC PROC FAR
-	mov dx, OFFSET MESSAGE
-	push ax
-	push bx
-	push cx
-	push dx
-	push ds
-	start:
+	jmp process
+	KEEP_SS DW 0
+	KEEP_SP DW 0
+	MESSAGE DB 'YES!', 0dh, 0ah, '$'
+	FINALLY DB 'Program Finished!', 0dh, 0ah, '$'
+	COUNTER DW 6
+	func_stack DW 40 DUP (?)
+	process:
+		mov KEEP_SS, SS
+    	mov KEEP_SP, SP
+    	mov ax, SEG func_stack
+    	mov ss, ax
+    	mov sp, OFFSET process
+		push ax
+		push bx
+		push cx
+		push dx
+		push ds
+		mov ax, SEG func_stack
+		mov ds, ax
+		mov dx, OFFSET MESSAGE
+		start:
+			call WriteMsg
+			sub COUNTER, 1
+			cmp COUNTER, 0
+			jnz start
+		mov COUNTER, 6
+		mov ax, DATA
+		mov dx, ax
+		mov cx, 0033h 
+		mov dx, 00FFh
+		mov ah, 86h
+		int 15h
+		mov dx, OFFSET FINALLY
 		call WriteMsg
-		sub COUNTER, 1
-		cmp COUNTER, 0
-		jnz start
-	mov cx, 0033h 
-	mov dx, 00FFh
-	mov ah, 86h
-	int 15h
-	mov dx, OFFSET FINALLY
-	call WriteMsg
-	pop ax
-   	pop bx
-   	pop cx
-   	pop dx
-   	pop ds
-   	mov al, 20h
-   	out 20h, al
-    iret
+		pop ax
+	   	pop bx
+	   	pop cx
+	   	pop dx
+	   	pop ds
+	   	mov ss, KEEP_SS
+	   	mov sp, KEEP_SP
+	   	mov al, 20h
+	   	out 20h, al
+		iret
 FUNC ENDP
 
 MAIN PROC FAR
@@ -70,9 +87,12 @@ MAIN PROC FAR
     begin:
     	mov ah, 0
 		int 16h
+		cmp al, 'q'
+		je quit
 		cmp al, 3
 		jnz begin
-		int 23h	
+		int 23h
+		jmp begin
 	quit:
     	cli
     	push ds
