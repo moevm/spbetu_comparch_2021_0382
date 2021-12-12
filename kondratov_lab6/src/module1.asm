7f86d5dc5ecec8504efe896816229dab8eba1a1f
YuriiKondratov <71669375+YuriiKondratov@users.noreply.github.com>
2021-12-12 15:36:55 +0300

    done

---
+++ kondratov_lab6/src/module1.asm
@@ -0,0 +1,19 @@
+.MODEL FLAT, C
+.CODE
+
+PUBLIC C unit_distribution
+unit_distribution PROC C numbers: dword, n: dword, res: dword, xmin: dword
+	mov esi, numbers
+	mov edi, res
+	mov ecx, n
+start_loop:
+	mov eax, [esi]
+	sub eax, xmin
+	mov ebx, [edi + 4*eax]
+	add ebx, 1
+	mov [edi + 4*eax], ebx
+	add esi, 4
+	loop start_loop
+ret
+unit_distribution ENDP
+END
---
+++ kondratov_lab6/src/module2.asm
@@ -0,0 +1,35 @@
+.MODEL FLAT, C
+.CODE
+
+PUBLIC C intervals_distribution
+intervals_distribution PROC C intervals: dword, n_int: dword, units: dword, n_units: dword, x_min: dword, res: dword
+	mov esi, intervals
+	mov edi, res
+	mov ecx, n_int
+start_loop:
+	mov eax, [esi]
+	add esi, 4h
+	mov ebx, [esi]
+	sub ebx, eax
+	sub eax, x_min
+
+	push ecx
+	push esi
+	
+	mov ecx, ebx
+	mov ebx, 0h
+	mov esi, units
+	start_loop2:
+		add ebx, [esi + eax*4]
+		add eax, 1
+		loop start_loop2
+	mov [edi], ebx
+	add edi, 4h
+
+	pop esi
+	pop ecx
+
+	loop start_loop
+ret
+intervals_distribution ENDP
+END
