1d3be1c0f16906cfb2a5bbe41ec715a8e4aa6999
YuriiKondratov <71669375+YuriiKondratov@users.noreply.github.com>
2021-12-16 21:02:04 +0300

    edit main

--- kondratov_lab6/src/main.cpp
+++ kondratov_lab6/src/main.cpp
@@ -1,11 +1,11 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
 #include <random>
 #include <string>
 
 using namespace std;
 
-extern "C" void unit_distribution(int* numbers, int n, int* res, int x_min);
+extern "C" void unit_distribution(int* numbers, int n, int* res, int x_min, float* mid);
 extern "C" void intervals_distribution(int* intervals, int n_int, int* units, int n_units, int x_min, int* res);
 
 int main() {
@@ -21,22 +21,20 @@ int main() {
 		return 0;
 	}
 	cout << "Enter Lgi seperated by spaces:" << endl;
-	auto intervals = new int[n_int];
-	for (int i = 0; i < n_int + 1; ++i) {
+	auto intervals = new int[n_int + 1];
+	for (int i = 0; i < n_int; ++i) {
 		cin >> intervals[i];
-		if (intervals[i] < x_min){
+		if (intervals[i] <= x_min){
 			cout << "lg" << i << " <= X_min" << endl;
 			return 0;
 		}
 	}
-	if (intervals[n_int - 1] > x_max) {
-		cout << "RBLast > X_max" << endl;
-		return 0;
-	}
+	intervals[n_int] = x_max;
 
 
 	double mean = (x_min + x_max) / 2;
 	double sigma = (x_max - x_min) / 4;
+	if (!sigma) sigma = 1;
 	cout << "\nNumbers generated with normal distribution (mean = " << mean << ", sigma = " << sigma << ").\n" << endl;
 	random_device r_d;
 	mt19937 generator(r_d());
@@ -48,25 +46,28 @@ int main() {
 		while (number < x_min || number > x_max)
 			number = distribution(generator);
 		numbers[i] = number;
-//		cout << number << " ";
-//		if ((i + 1) % 50 == 0) cout << "\n";
+		cout << number << " ";
+		if ((i + 1) % 50 == 0) cout << "\n";
 	}
+	cout << endl;
 
-	auto units = new int[x_max - x_min];
-	auto result = new int[n_int];
-	for (int i = 0; i < x_max - x_min; ++i)
+	auto units = new int[x_max - x_min + 1];
+	auto result = new int[n_int + 1];
+	for (int i = 0; i < x_max - x_min + 1; ++i)
 		units[i] = 0;
-	for (int i = 0; i < n_int; ++i)
+	for (int i = 0; i < n_int + 1; ++i)
 		result[i] = 0;
-	unit_distribution(numbers, n, units, x_min);
-	intervals_distribution(intervals, n_int, units, x_max - x_min, x_min, result);
+	float mid = 0.0;
+	unit_distribution(numbers, n, units, x_min, &mid);
+	intervals_distribution(intervals, n_int, units, x_max - x_min + 1, x_min, result);
+	cout << "mid: " << mid << "\n\n";
 
 	ofstream file("table.txt");
 	auto head = "N\tLeft border\tAmount of umbers";
 	file << head << endl;
 	cout << head << endl;
 	for (int i = 0; i < n_int; i++) {
-		auto row = to_string(i) + "\t" + to_string(intervals[i]) + "\t\t" + to_string(result[i]) + "\n";
+		auto row = to_string(i + 1) + "\t" + to_string(intervals[i]) + "\t\t" + to_string(result[i]) + "\n";
 		file << row;
 		cout << row;
 	}
