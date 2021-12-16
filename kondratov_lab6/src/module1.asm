4543526544c34216adb67ba71323a1b6075ef900
YuriiKondratov <71669375+YuriiKondratov@users.noreply.github.com>
2021-12-16 21:02:23 +0300

    edit module 1

--- kondratov_lab6/src/module1.asm
+++ kondratov_lab6/src/module1.asm
@@ -1,12 +1,17 @@
 .MODEL FLAT, C
+.DATA
+	
 .CODE
 
 PUBLIC C unit_distribution
-unit_distribution PROC C numbers: dword, n: dword, res: dword, xmin: dword
+unit_distribution PROC C numbers: dword, n: dword, res: dword, xmin: dword, mid: dword 
 	mov esi, numbers
 	mov edi, res
 	mov ecx, n
+	mov edx, 0h
+
 start_loop:
+	add edx, [esi]
 	mov eax, [esi]
 	sub eax, xmin
 	mov ebx, [edi + 4*eax]
@@ -14,6 +19,14 @@ start_loop:
 	mov [edi + 4*eax], ebx
 	add esi, 4
 	loop start_loop
+
+	mov edi, mid
+	mov [edi], edx
+
+	fild dword ptr [edi]
+	fild dword ptr n
+	fdivp
+	fst dword ptr [edi]
 ret
 unit_distribution ENDP
 END
